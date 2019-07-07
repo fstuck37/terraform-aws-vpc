968c48bb268bfede978aa453da69e1160baf40c1
fstuck37 <fstuck@gmail.com>
2019-07-07 14:30:33 -0400

    initialize

--- outputs.tf
+++ outputs.tf
@@ -9,27 +9,33 @@ output "vpc_name" {
 }
 
 output "subnet_ids" {
+  description = "Map with keys based on the subnet names and values of subnet IDs"
   value = "${zipmap(data.template_file.subnet-name.*.rendered, aws_subnet.subnets.*.id)}"
 }
 
 
 output "map_subnet_id_list" {
+  description = "Map with keys the same as subnet-order and values a list of subnet IDs"
   value = "${ zipmap(var.subnet-order, chunklist(aws_subnet.subnets.*.id, local.num-availbility-zones) )}"
 }
 
 output "pubrt_id" {
+  description = "The ID of the public routing table"
   value = "${(join("",aws_route_table.pubrt.*.id))}" 
 }
 
 output "privrt_id" {
+  description = "List of IDs of the private routing tables"
   value =  ["${aws_route_table.privrt.*.id}"]
 }
 
 output "vgw_id" {
+  description = "The ID of the VPN Gateway."
   value = "${aws_vpn_gateway.vgw.id}"
 }
 
 output "peerlink_ids" {
+  description = "Map with keys the same as the peer_requester variable and a value of the ID of the VPC Peering Connection."
   value = "${zipmap(keys(var.peer_requester), aws_vpc_peering_connection.peer.*.id) }"
 }
 
