9cdb0836c1a3d8a9161af096e9ef814dee56f3c3
fstuck37 <fstuck@gmail.com>
2019-12-15 20:34:56 -0500

    removed quotes from interpolation syntax

--- subnets.tf
+++ subnets.tf
@@ -11,15 +11,15 @@
 ##################################################
 
 resource "aws_subnet" "subnets" {
-  count = "${ length(var.subnets)*local.num-availbility-zones  }"
-  vpc_id = "${aws_vpc.main_vpc.id}"
-  cidr_block = "${cidrsubnet(var.subnets[element(local.subnet-order,local.subnets-list[count.index])],ceil(log(length(var.zones[var.region]),2)),local.azs-list[count.index])}"
-  availability_zone = "${element(var.zones[var.region],local.azs-list[count.index])}"
-  tags = "${merge(var.tags, map("Name", format("%02s", "${var.name-vars["account"]}-${var.name-vars["name"]}-${element(local.subnet-order,local.subnets-list[count.index])}-az-${element(split("-", element(var.zones[var.region],local.azs-list[count.index])), length(split("-", element(var.zones[var.region],local.azs-list[count.index]))) - 1)}")))}"  
+  count             = length(var.subnets)*local.num-availbility-zones
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = cidrsubnet(var.subnets[element(local.subnet-order,local.subnets-list[count.index])],ceil(log(length(var.zones[var.region]),2)),local.azs-list[count.index])
+  availability_zone = element(var.zones[var.region],local.azs-list[count.index])
+  tags              = merge(var.tags, map("Name", format("%02s", "${var.name-vars["account"]}-${var.name-vars["name"]}-${element(local.subnet-order,local.subnets-list[count.index])}-az-${element(split("-", element(var.zones[var.region],local.azs-list[count.index])), length(split("-", element(var.zones[var.region],local.azs-list[count.index]))) - 1)}")))
 }
 
 data "template_file" "subnets-tags" {
-  count = "${ length(var.subnets)*local.num-availbility-zones  }"
+  count    = length(var.subnets)*local.num-availbility-zones
   template = "${format("%02s", "${var.name-vars["account"]}-${var.name-vars["name"]}-${element(local.subnet-order,local.subnets-list[count.index])}-az-${element(split("-", element(var.zones[var.region],local.azs-list[count.index])), length(split("-", element(var.zones[var.region],local.azs-list[count.index]))) - 1)}")}"
 }
 
