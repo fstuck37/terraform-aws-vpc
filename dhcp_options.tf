ccaf7b57f18d77fccb936eaf9e27db93e9668a2c
fstuck37 <fstuck@gmail.com>
2019-12-15 20:12:03 -0500

    removed quotes from interpolation syntax

--- dhcp_options.tf
+++ dhcp_options.tf
@@ -11,13 +11,13 @@
 ##################################################
 
 resource "aws_vpc_dhcp_options" "dhcp-opt" {
-  domain_name          = "${var.domain_name}"
-  domain_name_servers  = "${var.domain_name_servers}"
-  ntp_servers          = "${var.ntp_servers}"
-  tags = "${merge(var.tags,map("Name",format("%s","${var.name-vars["account"]}-${replace(var.region,"-", "")}-${var.name-vars["name"]}-dhcp-options")))}"
+  domain_name          = var.domain_name
+  domain_name_servers  = var.domain_name_servers
+  ntp_servers          = var.ntp_servers
+  tags = merge(var.tags,map("Name",format("%s","${var.name-vars["account"]}-${replace(var.region,"-", "")}-${var.name-vars["name"]}-dhcp-options")))
 }
 
 resource "aws_vpc_dhcp_options_association" "dns_resolver" {
-  vpc_id          = "${aws_vpc.main_vpc.id}"
-  dhcp_options_id = "${aws_vpc_dhcp_options.dhcp-opt.id}"
+  vpc_id          = aws_vpc.main_vpc.id
+  dhcp_options_id = aws_vpc_dhcp_options.dhcp-opt.id
 }
