30afcb8feedb816b32c326e2162770a335050267
fstuck37 <fstuck@gmail.com>
2019-12-15 20:07:34 -0500

    removed quotes in depends_on reference

--- route-tables.tf
+++ route-tables.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "privrt" {
   vpc_id = "${aws_vpc.main_vpc.id}"
   propagating_vgws = ["${aws_vpn_gateway.vgw.id}"]
   tags = "${merge(var.tags,map("Name",format("%s","${var.name-vars["account"]}-${var.name-vars["name"]}-prod-az-${element(split("-", element(var.zones[var.region],count.index)), length(split("-", element(var.zones[var.region],count.index))) - 1)}")))}"
-  depends_on = ["aws_vpn_gateway.vgw"]
+  depends_on = [aws_vpn_gateway.vgw]
 }
 
 resource "aws_route_table_association" "associations" {
