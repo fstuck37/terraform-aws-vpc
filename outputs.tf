14cf23812c0987f2ff12b52d6de8d6a4412923a3
Stuck <StuckF@dnb.com>
2019-12-17 07:05:24 -0500

    test

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value = "${aws_vpc.main_vpc.id}"
+  value = aws_vpc.main_vpc.id
 }
 
 output "vpc_name" {
@@ -10,41 +10,35 @@ output "vpc_name" {
 
 output "subnet_ids" {
   description = "Map with keys based on the subnet names and values of subnet IDs"
-  value = "${zipmap(data.template_file.subnet-name.*.rendered, aws_subnet.subnets.*.id)}"
+  value = zipmap(data.template_file.subnet-name.*.rendered, aws_subnet.subnets.*.id)
 }
 
 
 output "map_subnet_id_list" {
   description = "Map with keys the same as subnet-order and values a list of subnet IDs"
-  value = "${ zipmap(var.subnet-order, chunklist(aws_subnet.subnets.*.id, local.num-availbility-zones) )}"
+  value = zipmap(var.subnet-order, chunklist(aws_subnet.subnets.*.id, local.num-availbility-zones))
 }
 
 output "pubrt_id" {
   description = "The ID of the public routing table"
-  value = "${(join("",aws_route_table.pubrt.*.id))}" 
+  value = (join("",aws_route_table.pubrt.*.id))
 }
 
 output "privrt_id" {
   description = "List of IDs of the private routing tables"
-  value =  ["${aws_route_table.privrt.*.id}"]
+  value = [aws_route_table.privrt.*.id]
 }
 
 output "vgw_id" {
   description = "The ID of the VPN Gateway."
-  value = "${aws_vpn_gateway.vgw.id}"
+  value = aws_vpn_gateway.vgw.id
 }
 
 output "peerlink_ids" {
   description = "Map with keys the same as the peer_requester variable and a value of the ID of the VPC Peering Connection."
-  value = "${zipmap(keys(var.peer_requester), aws_vpc_peering_connection.peer.*.id) }"
+  value = zipmap(keys(var.peer_requester), aws_vpc_peering_connection.peer.*.id)
 }
 
-
-
-
-output "azs-list" {
-  value = local.azs-list
-}
 
 output "subnets-list" {
   value = local.subnets-list
