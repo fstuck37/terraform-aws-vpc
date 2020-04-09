dbb20efa0dce39a485f7d729108c9aa29f978a92
stuckf <StuckF@dnb.com>
2020-04-08 22:13:04 -0400

    Route52 items

--- route53.tf
+++ route53.tf
@@ -1,4 +1,4 @@
-data "template_file" "subnet-24s-lists" {
+sdata "template_file" "subnet-24s-lists" {
   count    = length(var.vpc-cidrs)
   template = replace(join(",", slice(local.basecount, 0, data.template_file.subnet-24s-count.*.rendered[count.index])), "A", var.vpc-cidrs[count.index])
 }
@@ -8,19 +8,23 @@ data "template_file" "subnet-24s-count" {
   template = pow(2,(24 - element(split("/", var.vpc-cidrs[count.index]), 1)))
 }
 
+
+
 resource "aws_route53_zone" "reverse_zones" {
   count = var.default_reverse_zones ? length( local.route53-zones ) : 0
-  name  = "${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),2)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),1)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),0)}.in-addr.arpa"
+  name = "${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),2)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),1)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),0)}.in-addr.arpa"
   vpc {
     vpc_id = aws_vpc.main_vpc.id
   }
 }
+
 
 /*
 
 index element(split("|",local.route53-zones), 0)
 subnet element(split("|",local.route53-zones), 1)
 
+
 resource "aws_route53_zone" "reverse_zones" {
   count = var.default_reverse_zones ? length( local.route53-zones ) : 0
   name  = "${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),2)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),1)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones), 1), (24 - element(split("/", element(split("|",local.route53-zones), 1)), 1)), element(split("|",local.route53-zones), 0))),0)),0)}.in-addr.arpa"
@@ -28,7 +32,6 @@ resource "aws_route53_zone" "reverse_zones" {
     vpc_id = aws_vpc.main_vpc.id
   }
 }
-
 
 data "aws_route53_resolver_rules" "shared_resolver_rule"{
   count        = var.shared_resolver_rule ? 1 : 0
