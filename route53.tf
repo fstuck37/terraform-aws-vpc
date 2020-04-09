f99664f414792778ec3871dd487838a31243adda
stuckf <StuckF@dnb.com>
2020-04-08 21:22:55 -0400

    Route52 items

--- route53.tf
+++ route53.tf
@@ -1,22 +1,15 @@
-data "template_file" "subnet-zone-size" {
-  count    = length(var.vpc-cidrs)
-  template = pow(2,(24 - element(split("/", var.vpc-cidrs[count.index]), 1))) + (count.index==0 ? 0 : data.template_file.subnet-zone-size.*.rendered[count.index - 1])
+resource "aws_route53_zone" "reverse_zones" {
+  for_each = zipmap(var.vpc_cidrs, var.vpc_cidrs)
+    count = var.default_reverse_zones ? pow(2,(24 - element(split("/", each.key), 1))) :0 
+    name  = "${element(split(".",element(split("/", cidrsubnet(each.key, (24 - element(split("/", each.key), 1)), count.index)),0)),2)}.${element(split(".",element(split("/", cidrsubnet(each.key, (24 - element(split("/", each.key), 1)), count.index)),0)),1)}.${element(split(".",element(split("/", cidrsubnet(each.key, (24 - element(split("/", each.key), 1)), count.index)),0)),0)}.in-addr.arpa"
+    vpc {
+      vpc_id = aws_vpc.main_vpc.id
+    }
 }
 
 
 
 /*
-resource "aws_route53_zone" "reverse_zones" {
-  count = var.default_reverse_zones ? pow(2,(24 - element(split("/", var.vpc_cidr), 1))) :0 
-  name  = "${element(split(".",element(split("/", cidrsubnet(var.vpc_cidr, (24 - element(split("/", var.vpc_cidr), 1)), count.index)),0)),2)}.${element(split(".",element(split("/", cidrsubnet(var.vpc_cidr, (24 - element(split("/", var.vpc_cidr), 1)), count.index)),0)),1)}.${element(split(".",element(split("/", cidrsubnet(var.vpc_cidr, (24 - element(split("/", var.vpc_cidr), 1)), count.index)),0)),0)}.in-addr.arpa"
-  vpc {
-    vpc_id = aws_vpc.main_vpc.id
-  }
-}
-
-
-
-
 data "aws_route53_resolver_rules" "shared_resolver_rule"{
   count        = var.shared_resolver_rule ? 1 : 0
   share_status = "SHARED_WITH_ME"
