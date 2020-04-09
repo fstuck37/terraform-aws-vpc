ad4b984c2dd25962bebd000c82aef0108314e9a1
stuckf <StuckF@dnb.com>
2020-04-09 07:23:52 -0400

    Route52 items

--- route53.tf
+++ route53.tf
@@ -16,22 +16,6 @@ resource "aws_route53_zone" "reverse_zones" {
   }
 }
 
-
-/*
-
-
-index element(split("|",local.route53-zones[count.index]), 0)
-subnet element(split("|",local.route53-zones[count.index]), 1)
-
-
-resource "aws_route53_zone" "reverse_zones" {
-  count = var.default_reverse_zones ? length( local.route53-zones ) : 0
-  name = "${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones[count.index]), 1), (24 - element(split("/", element(split("|",local.route53-zones[count.index]), 1)), 1)), element(split("|",local.route53-zones[count.index]), 0))),0)),2)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones[count.index]), 1), (24 - element(split("/", element(split("|",local.route53-zones[count.index]), 1)), 1)), element(split("|",local.route53-zones[count.index]), 0))),0)),1)}.${element(split(".",element(split("/", cidrsubnet(element(split("|",local.route53-zones[count.index]), 1), (24 - element(split("/", element(split("|",local.route53-zones[count.index]), 1)), 1)), element(split("|",local.route53-zones[count.index]), 0))),0)),0)}.in-addr.arpa"
-  vpc {
-    vpc_id = aws_vpc.main_vpc.id
-  }
-}
-
 data "aws_route53_resolver_rules" "shared_resolver_rule"{
   count        = var.shared_resolver_rule ? 1 : 0
   share_status = "SHARED_WITH_ME"
@@ -42,5 +26,3 @@ resource "aws_route53_resolver_rule_association" "rule_association_0"{
   resolver_rule_id = element(data.aws_route53_resolver_rules.shared_resolver_rule.resolver_rule_ids, count.index)
   vpc_id           = aws_vpc.main_vpc.id
 }
-
-*/
