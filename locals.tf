366876f29caeb6fa6b4d2d0fc02e204aaa955c2f
Helder Feixas <root@USSHO820559.dnbint.net>
2020-04-15 16:14:21 -0400

    Fixed Name

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   peerlink_accepter_routes = flatten([
   for rt in aws_route_table.privrt : [
     for key, value in var.peer_accepter : {
-      name        = "${rt.id}-${element(split("|", value),2)}" 
+      name        = key 
       route_table = rt.id
       conn_id     = element(split("|", value),2)
       cidr        = element(split("|", value),1)
@@ -13,7 +13,7 @@ locals {
   peerlink_requester_routes = flatten([
   for rt in aws_route_table.privrt : [
     for key, value in var.peer_requester : {
-      name            = "${rt.id}-${element(split("|", value),2)}" 
+      name            = key 
       route_table     = rt.id
       account         = element(split("|", value),1)
       vpc_id          = element(split("|", value),2)
