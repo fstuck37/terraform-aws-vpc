f0c730817d40ca4fd70d0fb3c18af8c952c47542
Helder Feixas <root@USSHO820559.dnbint.net>
2020-04-15 16:07:02 -0400

    Testing Requestor Routes

--- peerlink.tf
+++ peerlink.tf
@@ -53,5 +53,5 @@ resource "aws_route" "requester_routes" {
   for_each                  = {for route in local.peerlink_requester_routes : route.name => route}
   route_table_id            = each.value.route_table
   destination_cidr_block    = each.value.cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.each.value.name.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.[each.value.name].id
 }
