3645b0f94a05af711ae28058da11e2427d8591e4
Stuck <StuckF@dnb.com>
2020-06-21 20:19:45 -0400

    vpn

--- vpn_connection.tf
+++ vpn_connection.tf
@@ -1,8 +1,20 @@
+svariable "default_vpn_connections" {
+  type = map(string)
+  default = { 
+      static_routes_only = false
+      destination_cidr_blocks = ""
+      tunnel1_inside_cidr = null
+      tunnel1_preshared_key = null
+      tunnel2_inside_cidr = null
+      tunnel2_preshared_key = null
+  }
+}
+
 resource "aws_customer_gateway" "aws_customer_gateways" {
-  for_each = var.vpc_connections
+  for_each = var.vpn_connections
     type = "ipsec.1"
-    bgp_asn = each.value.bgp_asn
-    ip_address = each.value.peer_ip_address
+    bgp_asn = merge(var.default_vpn_connections, each.value).bgp_asn
+    ip_address = merge(var.default_vpn_connections, each.value).peer_ip_address
     tags = merge(
       var.tags,
       map("Name",each.key)
@@ -10,14 +22,14 @@ resource "aws_customer_gateway" "aws_customer_gateways" {
 }
 
 resource "aws_vpn_connection" "aws_vpn_connections" {
-  for_each = var.vpc_connections
+  for_each = var.vpn_connections
     type = "ipsec.1"
     customer_gateway_id   = aws_customer_gateway.aws_customer_gateways[each.key].id
-    static_routes_only    = each.value.static_routes_only
-    tunnel1_inside_cidr   = each.value.tunnel1_inside_cidr == "" ? null : each.value.tunnel1_inside_cidr
-    tunnel1_preshared_key = each.value.tunnel1_preshared_key == "" ? null : each.value.tunnel1_preshared_key
-    tunnel2_inside_cidr   = each.value.tunnel2_inside_cidr == "" ? null : each.value.tunnel2_inside_cidr
-    tunnel2_preshared_key = each.value.tunnel2_preshared_key == "" ? null : each.value.tunnel2_preshared_key
+    static_routes_only    = merge(var.default_vpn_connections, each.value).static_routes_only
+    tunnel1_inside_cidr   = merge(var.default_vpn_connections, each.value).tunnel1_inside_cidr == "" ? null : merge(var.default_vpn_connections, each.value).tunnel1_inside_cidr
+    tunnel1_preshared_key = merge(var.default_vpn_connections, each.value).tunnel1_preshared_key == "" ? null : merge(var.default_vpn_connections, each.value).tunnel1_preshared_key
+    tunnel2_inside_cidr   = merge(var.default_vpn_connections, each.value).tunnel2_inside_cidr == "" ? null : merge(var.default_vpn_connections, each.value).tunnel2_inside_cidr
+    tunnel2_preshared_key = merge(var.default_vpn_connections, each.value).tunnel2_preshared_key == "" ? null : merge(var.default_vpn_connections, each.value).tunnel2_preshared_key
 
     tags = merge(
       var.tags,
@@ -30,3 +42,7 @@ resource "aws_vpn_connection_route" "aws_vpn_connection_routes" {
   vpn_connection_id = aws_vpn_connection.aws_vpn_connections[local.vpn_connection_routes.*.name[count.index]].id
   destination_cidr_block = local.vpn_connection_routes.*.cidr[count.index]
 }
+
+
+
+
