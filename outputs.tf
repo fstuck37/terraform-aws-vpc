cc7e5e9c3c79ccfec7e7ea9439b6c77520d927cf
Helder Feixas <feixash@dnb.com>
2020-10-13 14:59:34 -0400

    Update

--- outputs.tf
+++ outputs.tf
@@ -49,7 +49,7 @@ output "peerlink_ids" {
     ))
 }
 output "route_53_rules" {
-  value = flatten(data.aws_route53_resolver_rules.shared_resolver_rule)
+  value = flatten(data.aws_route53_resolver_rules)
 }
 
 output "aws_ec2_transit_gateway_vpc_attachment" {
