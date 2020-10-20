ebe783379da87951638d79f1fe380982e6c489df
Stuck <StuckF@dnb.com>
2020-10-20 10:40:23 -0400

    TXGW Routes for_each

--- txgw.tf
+++ txgw.tf
@@ -8,8 +8,8 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
 
 resource "aws_route" "txgw-routes" {
   for_each               = {for item in local.txgw_routes : item.name => item}
-  route_table_id         = each.route_table
-  destination_cidr_block = each.route
+  route_table_id         = each.item.route_table
+  destination_cidr_block = each.item.route
   transit_gateway_id     = var.transit_gateway_id
 }
 
