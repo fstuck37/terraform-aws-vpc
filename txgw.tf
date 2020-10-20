32fa9c724a982c7d66097d32c84a8b2c55952bff
Stuck <StuckF@dnb.com>
2020-10-20 10:41:17 -0400

    TXGW Routes for_each

--- txgw.tf
+++ txgw.tf
@@ -8,13 +8,21 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
 
 resource "aws_route" "txgw-routes" {
   for_each               = {for item in local.txgw_routes : item.name => item}
-  route_table_id         = each.item.route_table
-  destination_cidr_block = each.item.route
+  route_table_id         = each.value.route_table
+  destination_cidr_block = each.value.route
   transit_gateway_id     = var.transit_gateway_id
 }
 
 
 /*
+resource "aws_route" "requester_routes" {
+  for_each                  = {for route in local.peerlink_requester_routes : route.name => route}
+  route_table_id            = each.value.route_table
+  destination_cidr_block    = each.value.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer[each.value.peer_link_name].id
+}
+
+
 resource "aws_route" "txgw-routes" {
   count                  = var.transit_gateway_id == false ? 0 : length(var.transit_gateway_routes) * local.num-availbility-zones
   
