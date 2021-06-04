0ca2bd42ecf5e6398e162cfae75b862c60760bac
helfei <33071525+hfeixas@users.noreply.github.com>
2021-06-04 12:53:56 -0400

    Update txgw.tf

--- txgw.tf
+++ txgw.tf
@@ -7,7 +7,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
 
 resource "aws_route" "txgw-routes" {
   for_each               = {for item in local.txgw_routes : item.name => item}
-  route_table_id         = each.value.route_table
+  route_table_id         = aws_route_table.privrt[each.value.route_table_id].id
   destination_cidr_block = each.value.route
   transit_gateway_id     = var.transit_gateway_id
-}
+  }
