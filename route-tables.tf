ea7c956c524c878592fb689e65ec5de7f99df4a9
Stuck <StuckF@dnb.com>
2021-06-23 17:16:43 -0400

    test part of GWLB subnet fix

--- route-tables.tf
+++ route-tables.tf
@@ -14,7 +14,7 @@
 ##################################################
 
 resource "aws_route_table" "pubrt" {
-  count  = !contains(keys(var.subnets), "pub") || var.deploy_gwep ? 0 : 1
+  count  = !contains(keys(var.subnets), "pub") ? 0 : 1
   vpc_id = aws_vpc.main_vpc.id
   tags   = merge(
     var.tags,
